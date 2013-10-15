75524f8c778cb7a9d5b7aeab3dc59a9a1dd0f125
UnstableMutex <stormbringer-s@mail.ru>
2013-10-15 04:17:12 -0700

    classes

---
+++ classesforfias.cs
@@ -0,0 +1,16 @@
+class actstat
+{
+    public int actstatid { get; set; }
+    public string name { get; set; }
+}
+
+  class centerst
+    {
+        public int centerstID { get; set; }
+        public string Name { get; set; }
+    }
+    class currentst
+    {
+        public int currentstid { get; set; }
+        public string Name { get; set; }
+    }
