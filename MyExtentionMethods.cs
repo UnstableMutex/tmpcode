97d53b8c7bfb878f47cf2032f0407019df56422e
UnstableMutex <stormbringer-s@mail.ru>
2013-11-25 21:37:57 -0800

    Update MyExtentionMethods.cs

--- MyExtentionMethods.cs
+++ MyExtentionMethods.cs
@@ -6,3 +6,23 @@
         }
 
     }
+     public static partial class Ext
+    {
+        public static string GetString(this DbDataReader reader, string fieldName)
+        {
+            return reader.GetString(reader.GetOrdinal(fieldName));
+        }
+
+        public static DateTime GetDateTime(this DbDataReader reader, string fieldName)
+        {
+            return reader.GetDateTime(reader.GetOrdinal(fieldName));
+        }
+
+        public static void AddIfNo<TKey, TVal>(this Dictionary<TKey, TVal> dic, TKey key, TVal val)
+        {
+            if (!dic.ContainsKey(key))
+            {
+                dic.Add(key, val);
+            }
+        }
+    }
