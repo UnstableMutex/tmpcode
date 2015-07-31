b32b77f6f6dcd3e75e2e1c6f5d5e245fb0f333eb
Dmitry <stormbringer-s@mail.ru>
2015-07-31 11:08:37 +0400

    Update MyExtentionMethods.cs
    added all idatarecord with default value

--- MyExtentionMethods.cs
+++ MyExtentionMethods.cs
@@ -40,7 +40,7 @@
             var ind = r.GetOrdinal(fieldName);
             return r.GetString(ind);
         }
-        public static string GetString(this IDataRecord r, string fieldName, string def)
+          public static string GetString(this IDataRecord r, string fieldName, string def)
         {
             var ind = r.GetOrdinal(fieldName);
             if (r.IsDBNull(ind))
@@ -50,6 +50,106 @@
             else
             {
                 return r.GetString(ind);
+            }
+        }
+
+        public static bool GetBoolean(this IDataRecord r, string fieldName, bool def)
+        {
+            var ind = r.GetOrdinal(fieldName);
+            if (r.IsDBNull(ind))
+            {
+                return def;
+            }
+            else
+            {
+                return r.GetBoolean(ind);
+            }
+        }
+
+        public static decimal GetDecimal(this IDataRecord r, string fieldName, decimal def)
+        {
+            var ind = r.GetOrdinal(fieldName);
+            if (r.IsDBNull(ind))
+            {
+                return def;
+            }
+            else
+            {
+                return r.GetDecimal(ind);
+            }
+        }
+        public static short GetInt16(this IDataRecord r, string fieldName, short def)
+        {
+            var ind = r.GetOrdinal(fieldName);
+            if (r.IsDBNull(ind))
+            {
+                return def;
+            }
+            else
+            {
+                return r.GetInt16(ind);
+            }
+        }
+        public static int GetInt32(this IDataRecord r, string fieldName, int def)
+        {
+            var ind = r.GetOrdinal(fieldName);
+            if (r.IsDBNull(ind))
+            {
+                return def;
+            }
+            else
+            {
+                return r.GetInt32(ind);
+            }
+        }
+
+        public static long GetInt64(this IDataRecord r, string fieldName, long def)
+        {
+            var ind = r.GetOrdinal(fieldName);
+            if (r.IsDBNull(ind))
+            {
+                return def;
+            }
+            else
+            {
+                return r.GetInt64(ind);
+            }
+        }
+        public static Guid GetGuid(this IDataRecord r, string fieldName, Guid def)
+        {
+            var ind = r.GetOrdinal(fieldName);
+            if (r.IsDBNull(ind))
+            {
+                return def;
+            }
+            else
+            {
+                return r.GetGuid(ind);
+            }
+        }
+        public static byte GetByte(this IDataRecord r, string fieldName, byte def)
+        {
+            var ind = r.GetOrdinal(fieldName);
+            if (r.IsDBNull(ind))
+            {
+                return def;
+            }
+            else
+            {
+                return r.GetByte(ind);
+            }
+        }
+        public static DateTime GetDateTime(this IDataRecord r, string fieldName, DateTime def)
+        {
+
+            var ind = r.GetOrdinal(fieldName);
+            if (r.IsDBNull(ind))
+            {
+                return def;
+            }
+            else
+            {
+                return r.GetDateTime(ind);
             }
         }
         public static bool GetBoolean(this IDataRecord r, string fieldName)
